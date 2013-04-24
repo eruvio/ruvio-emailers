a24a2f7404609f0766e2bc0c31c4d52194094fb7
Eric Ruvio <eruvio90@gmail.com>
2013-04-24 17:05:36 -0400

    added basic test for bounds test

--- emailers/map/redirect.php
+++ emailers/map/redirect.php
@@ -3,8 +3,27 @@
 include '../Mobile_Detect.php';
 $detect = new Mobile_Detect();
 
-die(var_dump($_SERVER['QUERY_STRING']));
+$regexCoordinates = '/\d+,\d+$/';
 
+$queryString = $_SERVER['QUERY_STRING'];
+
+preg_match($regexCoordinates, $queryString, $coordinates);
+$coordinates = array_map(function($a){ return intval($a); }, explode(',', $coordinates[0]));
+
+$threshholds = array(
+    'x1' => 235,
+    'x2' => 383,
+    'y1' => 493,
+    'y2' => 568
+);
+
+if($coordinates[0] >= $threshholds['x1'] && $coordinates[0] <= $threshholds['x2'] && $coordinates[1] >= $threshholds['y1'] && $coordinates[1] <= $threshholds['y2']){
+    header("Location: http://google.com");
+    exit();
+} else {
+    header("Location: http://hebsdigital.com");
+    exit();
+}
 
 if ($detect->isMobile()) {
     // Any mobile device.
